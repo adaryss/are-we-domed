2b588b2c794ce4678afa670164bac9dc69b6eb00
Adam Ryšánek <adamrysanek@Adam-MacBook-Pro.local>
2021-10-17 19:33:00 +0200

    Use variables from .env

--- src/fetcher/getAsteroids.js
+++ src/fetcher/getAsteroids.js
@@ -1,8 +1,10 @@
 import { error as asteroidsFetchingError, hasFetched as asteroidsHasFetched } from "../features/asteroidsSlice";
 
+const { REACT_APP_NASA_TOKEN } = process.env;
+
 export const getAsteroids = (payload) => {
 	return (dispatch) => {
-		return fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-10-06&api_key=8GoRmGXrEmhUridCE5aOjO7q24IHvM4FIe3262zD')
+		return fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-10-06&api_key=${REACT_APP_NASA_TOKEN}`)
 		.then((response) => {
 			
 			if (!response.ok) {
