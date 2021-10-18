b91dc6cb2b293d0ca84ab5c5de221efd6c380da8
Adam Ryšánek <adamrysanek@Adam-MacBook-Pro.local>
2021-10-18 21:44:55 +0200

    Add getting date func for api call

--- src/fetcher/getAsteroids.js
+++ src/fetcher/getAsteroids.js
@@ -1,10 +1,17 @@
 import { error as asteroidsFetchingError, hasFetched as asteroidsHasFetched } from "../features/asteroidsSlice";
 
+const getStartDate = () => {
+	const startDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+	const formattedStartDate = startDate.toISOString().slice(0, 10);
+
+	return formattedStartDate;
+}
+
 const { REACT_APP_NASA_TOKEN } = process.env;
 
 export const getAsteroids = (payload) => {
 	return (dispatch) => {
-		return fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-10-06&api_key=${REACT_APP_NASA_TOKEN}`)
+		return fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${getStartDate()}&api_key=${REACT_APP_NASA_TOKEN}`)
 		.then((response) => {
 			
 			if (!response.ok) {
