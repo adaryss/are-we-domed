de590118dbf17690d6fee7154f83f99315be3612
Adam Ryšánek <adamrysanek@Adam-MacBook-Pro.local>
2021-10-25 21:49:22 +0200

    Add fetching button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { getAsteroids } from './fetcher/getAsteroids';
-import { selectAsteroids } from './selectors/asteroids';
-import { useEffect } from 'react';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getAsteroids } from "./fetcher/getAsteroids";
+import { selectAsteroids } from "./selectors/asteroids";
+import { useEffect } from "react";
+import { SearchButton } from "./components/SearchButton/SearchButton";
+import { Container, Typography } from "@mui/material";
 
 const App = () => {
-  const asteroids = useSelector(selectAsteroids);
+  const { asteroidsData, error, isFetching } = useSelector(selectAsteroids);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log('asteroids', asteroids);
-  }, [asteroids]);
+    console.log("asteroids", asteroidsData);
+  }, [asteroidsData]);
 
   const handleButtonClick = () => {
     dispatch(getAsteroids());
-  }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={handleButtonClick}>Load asteroids</button>
-        <div>{JSON.stringify(asteroids)}</div>
-      </header>
-    </div>
+    <Container
+      maxWidth={false}
+      sx={{
+        minHeight: "100vh",
+        background: "#13294B",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Container maxWidth="md">
+        <Typography variant="h1" sx={{ color: "#cecece", textAlign: "center" }}>
+          Are we doomed?
+        </Typography>
+      </Container>
+      {Boolean(asteroidsData) ? (
+        <div>{JSON.stringify(asteroidsData)}</div>
+      ) : (
+        <SearchButton
+          handleButtonClick={handleButtonClick}
+          isFetching={isFetching}
+          hasError={error}
+        />
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
